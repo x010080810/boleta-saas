d5ca0f758e831d169b1a66a248c72dea8e54c62c
sigsda30 <sigsda30@minedu.gob.pe>
2026-07-11 22:41:51 -0500

    Force LF line endings in entrypoint.sh

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,4 +3,4 @@ set -e
 
 nohup celery -A app.core.celery_app worker -l info > /tmp/celery_worker.log 2>&1 &
 
-exec uvicorn app.main:app --host 0.0.0.0 --port "$PORT"
+exec uvicorn app.main:app --host 0.0.0.0 --port " \
