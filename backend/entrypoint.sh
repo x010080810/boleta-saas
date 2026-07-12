f4b70125b798d33d9aec083097b98eb5134f90b4
sigsda30 <sigsda30@minedu.gob.pe>
2026-07-11 22:52:09 -0500

    Fix entrypoint.sh bad escaping, use proper LF

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,4 +3,4 @@ set -e
 
 celery -A app.core.celery_app worker -l info --concurrency=1 --pool=solo > /tmp/celery_worker.log 2>&1 &
 
-exec uvicorn app.main:app --host 0.0.0.0 --port " \
+exec uvicorn app.main:app --host 0.0.0.0 --port "$PORT"
