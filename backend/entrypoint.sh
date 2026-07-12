4f4fe1a498fc3fcb1de7045f5a77c0344a474ed3
sigsda30 <sigsda30@minedu.gob.pe>
2026-07-11 22:44:41 -0500

    entrypoint.sh with celery solo pool (min RAM)

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-nohup celery -A app.core.celery_app worker -l info > /tmp/celery_worker.log 2>&1 &
+celery -A app.core.celery_app worker -l info --concurrency=1 --pool=solo > /tmp/celery_worker.log 2>&1 &
 
 exec uvicorn app.main:app --host 0.0.0.0 --port " \
